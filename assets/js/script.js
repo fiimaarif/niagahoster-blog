aef70c4086cde9950bb24f4c5a7b545aebec9d4c
fiimaarif <fiimaarif44@gmail.com>
2022-03-08 17:33:27 +0700

    update sticky

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
+let comment = document.querySelector(".comment-sidebar");
 
+document.querySelector("#comment").onclick = () => {
+  comment.classList.remove("d-none");
+  comment.classList.add("d-flex");
+};
+document.querySelector("#close").onclick = () => {
+  comment.classList.add("d-none");
+};
+window.onscroll = function () {
+  myFunction();
+};
+
+window.onscroll = () => {
+  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+  const scrolled = window.scrollY;
+  let sidebar = document.querySelector(".left-sidebar");
+  if (scrolled) {
+    sidebar.classList.remove("d-none");
+  }
+};
